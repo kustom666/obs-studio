3afbb6a06e915850a0ba69c0333045d7a36ad821
kustom666 <paul.forti@gmail.com>
2017-03-29 05:29:22 +0200

    msvc compat

--- plugins/enc-openh264/enc-openh264.c
+++ plugins/enc-openh264/enc-openh264.c
@@ -1,4 +1,5 @@
 #include <obs-module.h>
+#include <util/platform.h>
 #include "codec_api.h"
 #include "codec_def.h"
 
@@ -10,6 +11,7 @@ struct enc_openh264 {
     SEncParamBase param;
     int width;
     int height;
+	os_performance_token_t *performance_token;
 };
 
 #define do_log(level, format, ...) \
@@ -44,25 +46,43 @@ static void *openh264_create(obs_data_t *settings, obs_encoder_t *encoder)
     enc_oh264->width = (int)obs_encoder_get_width(encoder);
     enc_oh264->height = (int)obs_encoder_get_height(encoder);
     //enc_oh264->param.iTargetBitrate = (int)obs_data_get_int(settings, "bitrate");
-    enc_oh264->param.iTargetBitrate = 6000000;
+    enc_oh264->param.iTargetBitrate = 1000;
     //info("Width: %d - Height: %d", enc_oh264->param.iPicWidth, enc_oh264->param.iPicHeight);
     //enc_oh264->openh264_encoder->
     int videoFormat = videoFormatI420;
-    int bitrate = 6000000;
+    int bitrate = 1000;
     (*enc_oh264->openh264_encoder)->SetOption(enc_oh264->openh264_encoder,ENCODER_OPTION_DATAFORMAT, &videoFormat);
-    //(*enc_oh264->openh264_encoder)->SetOption(ENCODER_OPTION_MAX_BITRATE, &bitrate);
+    (*enc_oh264->openh264_encoder)->SetOption(enc_oh264->openh264_encoder,ENCODER_OPTION_MAX_BITRATE, &bitrate);
     (*enc_oh264->openh264_encoder)->Initialize(enc_oh264->openh264_encoder, &enc_oh264->param);
     info("Open h264 Encoder created");
+	enc_oh264->performance_token =
+		os_request_high_performance("openh264 encoding");
+
     return enc_oh264;
 }
 static void openh264_destroy(void *data)
 {
     struct enc_openh264 *enc_oh264 = data;
     if(enc_oh264){
+		os_end_high_performance(enc_oh264->performance_token);
         (*enc_oh264->openh264_encoder)->Uninitialize(enc_oh264->openh264_encoder);
         WelsDestroySVCEncoder(enc_oh264->openh264_encoder);
         bfree(enc_oh264);
     }
+}
+
+static void openh264_video_info(void *data, struct video_scale_info *info)
+{
+
+}
+
+static bool openh264_sei(void *data, uint8_t **sei, size_t *size)
+{
+	return true;
+}
+static bool openh264_extra_data(void *data, uint8_t **extra_data, size_t *size)
+{
+	return true;
 }
 static bool openh264_encode(void *data, struct encoder_frame *frame,
 		struct encoder_packet *packet, bool *received_packet)
@@ -83,11 +103,11 @@ static bool openh264_encode(void *data, struct encoder_frame *frame,
     pic.iPicWidth = enc_oh264->width;
     pic.iPicHeight = enc_oh264->height;
     pic.iColorFormat = videoFormatI420;
-    pic.iStride[0] = pic.iPicWidth;
-    pic.iStride[1] = pic.iStride[2] = pic.iPicWidth >> 1;
+    pic.iStride[0] = frame->linesize[0];
+    pic.iStride[1] = pic.iStride[2] = frame->linesize[1];
     pic.pData[0] = frame->data[0]; // pointer to frame data?
-    pic.pData[1] = pic.pData[0] + pic.iPicWidth * pic.iPicHeight;
-    pic.pData[2] = pic.pData[1] + (pic.iPicWidth * pic.iPicHeight >> 2);
+    pic.pData[1] = frame->data[1];
+    pic.pData[2] = frame->data[2];
     rv = (*enc_oh264->openh264_encoder)->EncodeFrame(enc_oh264->openh264_encoder, &pic, &info);
     if(rv != cmResultSuccess)
         warn("Couldn't encode the frame");
@@ -100,12 +120,13 @@ static bool openh264_encode(void *data, struct encoder_frame *frame,
     packet->type = OBS_ENCODER_VIDEO;
     packet->keyframe = 0;
 
-    *received_packet = (info.sLayerInfo[info.iLayerNum].iNalCount != 0);
+    *received_packet = true;
 
     return true;
 }
 static bool openh264_update(void *data, obs_data_t *settings)
 {
+	int bitrate = (int)obs_data_get_int(settings, "bitrate");
     return true;
 }
 static obs_properties_t *openh264_props(void *unused)
@@ -113,10 +134,12 @@ static obs_properties_t *openh264_props(void *unused)
 	UNUSED_PARAMETER(unused);
 
 	obs_properties_t *props = obs_properties_create();
+	obs_properties_add_int(props, "bitrate", "bitrate", 50, 10000000, 1);
     return props;
 }
 static void openh264_defaults(obs_data_t *settings)
 {
+	obs_data_set_default_int(settings, "bitrate", 2500);
 }
 struct obs_encoder_info openh264_encoder = {
 	.id             = "openh264",
@@ -129,9 +152,9 @@ struct obs_encoder_info openh264_encoder = {
 	.update         = openh264_update,
     .get_properties = openh264_props,
 	.get_defaults   = openh264_defaults,
-	// .get_extra_data = openh264_extra_data,
-	// .get_sei_data   = openh264_sei,
-	// .get_video_info = openh264_video_info
+	.get_extra_data = openh264_extra_data,
+	.get_sei_data   = openh264_sei,
+	.get_video_info = openh264_video_info
 };
 
 
